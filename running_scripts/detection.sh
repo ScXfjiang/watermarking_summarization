a7796f171e079d710e7fb5b7582f138f887285dc
scxfjiang <sc.xfjiang@gmail.com>
2023-08-04 04:39:16 +0100

    update

---
+++ running_scripts/detection.sh
@@ -0,0 +1,4 @@
+python ../detection.py \
+    --summary_path="/scratch/22200056/T5_pretrained_model/inference_log/cnn_T5_base_watermark_true/Aug-01-2023-21-28-01-4066f99d-5415-4212-bec0-4e441968eaad/summary_target.csv" \
+    --model_type="t5-base" \
+    --log_dir="T5_base_news_watermark_false"
